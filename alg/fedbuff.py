a6503d4c46648a1bb372df903e145c561582e8c2
Aditya Singh Waldia <asw3924@gmail.com>
2025-12-19 19:17:03 +0530

    Reverted to old fedbuff

--- alg/fedbuff.py
+++ alg/fedbuff.py
@@ -7,7 +7,6 @@ from utils.time_utils import time_record
 def add_args(parser):
     parser.add_argument('--etag', type=float, default=5)
     parser.add_argument('--k', type=int, default=10)
-    parser.add_argument('--alpha', type=float, default=0.5)
     return parser.parse_args()
 
 
@@ -23,7 +22,6 @@ class Server(AsyncBaseServer):
     def __init__(self, id, args, clients):
         super().__init__(id, args, clients)
         self.buffer = []
-        self.weight_buffer = []
 
     def run(self):
         self.sample()
@@ -34,25 +32,11 @@ class Server(AsyncBaseServer):
         self.update_status()
 
     def aggregate(self):
-        s = self.staleness[self.cur_client.id]
-        
-        # 2. Calculate the scaling weight (Polynomial Scaling is most common)
-        # weight = (1 + s)^-alpha
-        weight = (1 + s) ** (-self.args.alpha)
         self.buffer.append(self.cur_client.dW)
-        self.weight_buffer.append(weight)
+
         if len(self.buffer) == self.args.k:
             t_g = self.model2tensor()
-            
-            # 4. Use a weighted average instead of a simple mean
-            # Instead of torch.mean(buffer), we do sum(weighted_updates) / sum(weights)
-            weighted_update_sum = torch.stack(self.buffer).sum(dim=0)
-            total_weight = sum(self.weight_buffer)
-            
-            # Update global model: W = W + etag * (weighted_average)
-            t_g_new = t_g + self.args.etag * (weighted_update_sum / total_weight)
+            t_g_new = t_g + self.args.etag * torch.mean(torch.stack(self.buffer), dim=0) / self.args.k
             self.tensor2model(t_g_new)
 
-            # 5. Clear both buffers
             self.buffer = []
-            self.weight_buffer = []
